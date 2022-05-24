31884be25b98a937408fd96f4dc9a1e2f3af165b
Bruno Santos Vieira <75911878+BrunosVieira88@users.noreply.github.com>
2022-05-24 16:16:52 -0300

    first

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+$nomes = [
+    'bruno' => [
+        'nome'=> 'Bruno Santos Vieira',
+        'idade'=> 33 
+    ],
+    'victor' => [
+        'nome'=> 'victor',
+        'idade'=> 22
+    ],
+    'khalil' => [
+        'nome'=> 'khalil',
+        'idade'=> 19 
+    ]
+
+];
+
+$umaLista = ['um','dois','tres'];
+for ($i=0; $i < count($umaLista); $i++) { 
+   echo $umaLista[$i].PHP_EOL;
+}
+
+$umaLista[]='quatro';
+
+foreach ($nomes as $nomec => $nome   ) {
+    echo $nomec ." Seu nome completo é " . $nome['nome'];
+    echo " Com a idade de ". $nome['idade'].PHP_EOL;
+}
+
+$nomes['Wellington'] = [
+        'nome'=> 'Wellington Silva',
+        'idade'=> 40 
+];
